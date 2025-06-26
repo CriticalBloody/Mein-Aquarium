1c0ae5de4dc6fa48db18c48aa45fa9d89d925854
CriticalBloody <larstaube@yahoo.de>
2025-06-26 09:41:09 +0200

    Initial Commit

---
+++ Mein AquariumTests/Mein_AquariumTests.swift
@@ -0,0 +1,17 @@
+//
+//  Mein_AquariumTests.swift
+//  Mein AquariumTests
+//
+//  Created by Lars Taube on 26.06.25.
+//
+
+import Testing
+@testable import Mein_Aquarium
+
+struct Mein_AquariumTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
